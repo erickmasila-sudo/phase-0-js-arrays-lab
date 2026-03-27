ba2b5dcbc3a73549e7c9fa5e8b374554db17885c
erickmasila-sudo <erick.masila@student.moringaschool.com>
2026-03-27 11:22:32 +0300

    Final commit

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,6 +1,26 @@
 // Write your code here
+let products=["Laptop", "Phone", "Headphones", "Monitor"]
+function logFirstProduct(){
+ products;
+ console.log(products[0])
+}
 
-
+function updateProductName(i, name){
+   for(let num of products){
+    if(products.indexOf(num)==i){
+      products[i] = name
+    }
+   }
+   return products
+}
+function addProduct(ProductName){
+  products.push(ProductName)
+  return products
+}
+function removeLastProduct(){
+products.pop()
+return products
+}
 
 // Export the necessary parts for testing
 module.exports = {
